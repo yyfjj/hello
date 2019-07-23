9be9471d69e5381ff4e68cad288f0b188b5d50f5
yyfjj <yyfjj@foxmail.com>
2019-07-23 23:48:30 +0800

    Create EchoHello.php

---
+++ src/EchoHello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Filename:  EchoHello.php
+ * User:      cmder
+ * Date:      2018/3/25
+ * Time:      22:49
+ */
+namespace Hello;
+
+class EchoHello
+{
+    public static function say()
+    {
+        echo 'hello';
+    }
+}
